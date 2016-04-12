cb667741afa1e10d2d39ecbf9352aea2f9857e8e
vchavarria <vchavarria@CR0032.rule.lan>
2016-04-12 10:35:39 -0600

    First commit

---
+++ src/test/java/com/cucumber/testng/examples/BaseStepDefs.java
@@ -0,0 +1,57 @@
+package com.cucumber.testng.examples;
+
+import java.net.MalformedURLException;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import cucumber.api.Scenario;
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
+
+
+/**
+ * Created by amit.rawat on 21/12/15.
+ */
+
+	
+
+public class BaseStepDefs {
+	public static WebDriver driver;
+
+
+    @Before()
+    //public void before(Scenario scenario) {
+     //   System.out.println("This is before Scenario: " + scenario.getName().toString());
+        /**
+         * Delete all cookies at the start of each scenario to avoid
+         * shared state between tests
+         */
+  // }
+        public void openBrowser() throws MalformedURLException {
+		 System.out.println("Called openBrowser");
+		 //System.setProperty("webdriver.chrome.driver", "C://Users/vchavarria/Downloads/Maven-Failsafe-Plugin-Example-master/resources/com/drivers/chromedriver.exe");
+		 //driver = new ChromeDriver();
+	      	driver = new FirefoxDriver();
+	      	driver.manage().deleteAllCookies();
+	    	driver.manage().window().maximize();
+	    	driver.get("http://localhost:4002/#/app/fundCompanies/201");
+	    	driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	    	
+        
+    }
+
+      
+    @After
+    public void after(Scenario scenario) {
+        System.out.println("This is after Scenario: " + scenario.getName().toString());
+              driver.quit();
+    }
+   
+     
+    }
+  
+    
+
---
+++ src/test/resources/com.cucumber.testng.examples/OMmain.feature
@@ -0,0 +1,31 @@
+Feature: OMTest
+  This is the suite for OM Project
+
+  @basictest
+  Scenario: Validate the header
+    When Navigate to OM site
+    Then Header shall be "OPERATING METRICS"
+
+  @basictest
+  Scenario: Reviewing the table headers
+    When Navigate to OM site
+    Then I should be see the table header "Qualitative Company Details"
+
+  @basictest
+  Scenario: Updating Notes field
+    When Navigate to OM site
+    And user double clicks in Company notes
+    Then User is able to enter "Company note here for 401 test"
+    And user shall be able to click in save button
+
+  @wip
+  Scenario Outline: Validate Qualitative Company details Fields
+    When Navigate to OM site
+    And pass the xpath locator <xpath>
+    Then OMUser is able to see the <Fields>
+
+    Examples: 
+      | Fields        | xpath                                  |
+      | Notes         | //div[3]/div/div[2]/div/div[2]/span[5] |
+      | GICS Sector   | //div[2]/div[2]/span[5]                |
+      | GICS Industry | //div[3]/div[2]/span[5]                |
