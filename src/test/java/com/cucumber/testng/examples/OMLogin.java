f68333601fb61a152226723010f81c2c71f4a5f3
vchavarria <vchavarria@CR0032.rule.lan>
2016-04-21 10:24:53 -0600

    Updating login functionality

---
+++ src/test/java/com/cucumber/testng/examples/OMLogin.java
@@ -0,0 +1,54 @@
+package com.cucumber.testng.examples;
+
+import org.openqa.selenium.WebDriver;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class OMLogin {
+public WebDriver driver;
+	
+	
+	public OMLogin()
+	    {
+	    	driver = BaseStepDefs.driver;
+	    	
+	    }
+	
+	@When("^User enters a valid user name$")
+	public void user_enters_a_valid_user_name() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^User enters a valid password$")
+	public void user_enters_a_valid_password() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^user shall be redirected to the OM main page$")
+	public void user_shall_be_redirected_to_the_OM_main_page() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^User enters an invalid user name$")
+	public void user_enters_an_invalid_user_name() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^error message shall be showed$")
+	public void error_message_shall_be_showed() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^User enters an invalid password$")
+	public void user_enters_an_invalid_password() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+}
---
+++ src/test/resources/com.cucumber.testng.examples/Login.feature
@@ -0,0 +1,41 @@
+#Author: Victor Chavarria
+#Keywords Summary :
+#Feature: List of scenarios.
+#Scenario: Business rule through list of steps with arguments.
+#Given: Some precondition step
+#When: Some key actions
+#Then: To observe outcomes or validation
+#And,But: To enumerate more Given,When,Then steps
+#Scenario Outline: List of steps for data-driven as an Examples and <placeholder>
+#Examples: Container for s table
+#Background: List of steps run before each of the scenarios
+#""" (Doc Strings)
+#| (Data Tables)
+#@ (Tags/Labels):To group Scenarios 
+#<> (placeholder)
+#""
+## (Comments)
+
+#Sample Feature Definition Template
+
+Feature: Login functionality
+
+
+
+ @regression @login @positive
+  Scenario: Login with a valid user
+  When User enters a valid user name
+  And User enters a valid password 
+  Then user shall be redirected to the OM main page 
+  
+  @regression @login @negative
+  Scenario: Login with a invalid user
+  When User enters an invalid user name
+  And User enters a valid password 
+  Then error message shall be showed 
+  
+  @regression @login @negative
+  Scenario: Login with a invalid password
+  When User enters a valid user name
+  And User enters an invalid password 
+  Then error message shall be showed 
