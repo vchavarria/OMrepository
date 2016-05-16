f4002da23f14d3f94ab2598562bbdbb3eb67b7fd
unknown <vchavarria@CR0032.rule.lan>
2016-05-16 17:24:47 -0600

    Commit Reviewer queue validation

---
+++ src/test/java/Test_Steps/OMReviewer_queue.java
@@ -0,0 +1,68 @@
+package Test_Steps;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+import com.cucumber.testng.examples.BaseStepDefs;
+import com.cucumber.testng.examples.Login_Page_pageobj;
+import com.cucumber.testng.examples.Reviewer_Queue_pageobj;
+
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import junit.framework.Assert;
+
+public class OMReviewer_queue {
+	public WebDriver driver;
+	private StringBuffer verificationErrors = new StringBuffer();
+
+public OMReviewer_queue()
+{
+	driver = BaseStepDefs.driver;
+	
+}
+@Given("^User is on the Reviewer queue$")
+public void User_is_on_the_Reviewer_queue () throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+    //throw new PendingException();
+	Assert.assertEquals((Reviewer_Queue_pageobj.tab_Review(driver)).getText(), "REVIEW");
+	//Select oSelect = new Select(Reviewer_Queue_pageobj.ddwn_Filter1(driver));
+	//oSelect.selectByVisibleText("Status");
+}
+
+@Then("^User selects Filter by \"([^\"]*)\"$")
+public void user_selects_Filter_by (String FltStatus) throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+    //throw new PendingException();
+	Reviewer_Queue_pageobj.ddwn_Filter1(driver).click();
+	Select oSelect = new Select(Reviewer_Queue_pageobj.ddwn_Filter1(driver));
+	oSelect.selectByVisibleText("Status");
+}
+
+@And("^User selects the status \"([^\"]*)\"$")
+public void user_selects_the_status(String FndStatus) throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+    //throw new PendingException();
+	Reviewer_Queue_pageobj.ddwn_Filter1(driver).click();
+	Select oSelect = new Select(Reviewer_Queue_pageobj.ddwn_Filter1(driver));
+	oSelect.selectByVisibleText("Status");
+}
+
+@Then("^Only the firms in \"([^\"]*)\"$ are showed$")
+public void only_the_firms (String FndStatus2) throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+    //throw new PendingException();
+	Reviewer_Queue_pageobj.ddwn_Filter1(driver).click();
+	Select oSelect = new Select(Reviewer_Queue_pageobj.ddwn_Filter1(driver));
+	oSelect.selectByVisibleText("Status");
+}
+
+
+
+
+
+
+}
--- src/test/java/Utilities/Utility.java
+++ src/test/java/Utilities/Utility.java
@@ -39,4 +39,5 @@ public static WebDriver driver;
 			return false;
 		}
 	}
+	
 }
